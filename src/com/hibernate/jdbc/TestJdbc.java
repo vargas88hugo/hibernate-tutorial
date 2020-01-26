ca5f362412c8a7460afa97985497eea4e81cbd4c
Hugo Vargas <vargas88hugo@gmail.com>
2020-01-26 17:29:07 -0500

    [Feature] My first commit on this project and tested JDBC

---
+++ src/com/hibernate/jdbc/TestJdbc.java
@@ -0,0 +1,26 @@
+package com.hibernate.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+		String user = "hbstudent";
+		String pass = "hbstudent";
+		
+		try {
+			System.out.println("Connecting to Database: " + jdbcUrl);
+			
+			@SuppressWarnings("unused")
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			System.out.println("Connection Successfully!!!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
