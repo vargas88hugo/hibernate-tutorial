1d95f31c6a83d0c436fbbf5529a8541648ec6ccc
Hugo Vargas <vargas88hugo@gmail.com>
2020-01-26 19:44:54 -0500

    [Feature] Created and saved java objects in db and implemented primary keys

---
+++ src/com/tutorial/hibernate/demo/CreateStudentDemo.java
@@ -0,0 +1,43 @@
+package com.tutorial.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.tutorial.hibernate.demo.entity.Student;
+
+public class CreateStudentDemo {
+	
+	public static void main(String[] args) {
+		
+		// Create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+		
+		// Create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// Create a student object
+			System.out.println("Creating new student object...");
+			Student tempStudent = new Student("Paul", "Wall", "paulwall@email.com");
+			
+			// Start a transaction
+			session.beginTransaction();
+			
+			// Save the student object
+			System.out.println("Saving the student...");
+			session.save(tempStudent);
+			
+			// Commit transaction
+			session.getTransaction().commit();
+			System.out.println("Done!!!");
+			
+		} finally {
+			factory.close();
+		}
+	}
+
+}
---
+++ src/com/tutorial/hibernate/demo/PrimaryKeyDemo.java
@@ -0,0 +1,46 @@
+package com.tutorial.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.tutorial.hibernate.demo.entity.Student;
+
+public class PrimaryKeyDemo {
+
+	public static void main(String[] args) {
+		
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+		
+		// Create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// Create 3 student objects
+			System.out.println("Creating new student objects...");
+			Student tempStudent1 = new Student("Paul", "Wall", "paulwall@email.com");
+			Student tempStudent2 = new Student("Hugo", "Vargas", "hugo@email.com");
+			Student tempStudent3 = new Student("Mairelys", "Rondon", "mairelys@email.com");
+			
+			// Start a transaction
+			session.beginTransaction();
+			
+			// Save 3 student objects
+			System.out.println("Saving the students...");
+			session.save(tempStudent1);
+			session.save(tempStudent2);
+			session.save(tempStudent3);
+			
+			// Commit transaction
+			session.getTransaction().commit();
+			System.out.println("Done!!!");
+			
+		} finally {
+			factory.close();
+		}
+	}
+
+}
